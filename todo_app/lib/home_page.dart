61517b113a1ced7ff770ed971fb7edf1a9b2d552
jamescardona11 <jjcardonao@gmail.com>
2020-07-24 19:52:14 -0500

    Create a ui for list of todo's

--- todo_app/lib/home_page.dart
+++ todo_app/lib/home_page.dart
@@ -37,6 +37,7 @@ class HomePage extends StatelessWidget {
                   )
                 ],
               ),
+              child: _TodoContainer(),
             )
           ],
         ),
@@ -56,13 +57,52 @@ class _TodoContainer extends StatefulWidget {
 }
 
 class __TodoContainerState extends State<_TodoContainer> {
-  final List<Todo> listOfTodo = [];
+  final List<Todo> listOfTodo = [
+    Todo(name: 'make cookies', check: false),
+    Todo(name: 'buy a milk', check: true),
+    Todo(name: 'read news', check: false),
+    Todo(name: 'create a presentation', check: true),
+    Todo(name: 'go to the park', check: true),
+  ];
 
   @override
   Widget build(BuildContext context) {
     return ListView.builder(
       itemCount: listOfTodo.length,
-      itemBuilder: (context, index) => Container(),
+      itemBuilder: (context, index) => Container(
+        height: 65,
+        margin: EdgeInsets.symmetric(vertical: 5, horizontal: 10),
+        decoration: BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.all(Radius.circular(25)),
+          boxShadow: [
+            BoxShadow(
+              color: Colors.black54,
+              offset: Offset(0, 1),
+              spreadRadius: -3,
+              blurRadius: 8,
+            ),
+          ],
+        ),
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Text(
+                listOfTodo[index].name,
+                style: TextStyle(
+                  decoration: listOfTodo[index].check ? TextDecoration.lineThrough : TextDecoration.none,
+                ),
+              ),
+              Checkbox(
+                value: listOfTodo[index].check,
+                onChanged: (value) {},
+              ),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
